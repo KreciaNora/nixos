15cb59474824d4709b067a7fbb57ea25acc03102
KreciaNora <krecikowa01@gmail.com>
2026-01-07 01:21:19 +0100

    Sway config - update

--- home/sway.nix
+++ home/sway.nix
@@ -18,6 +18,21 @@
       keybindings = lib.mkOptionDefault {
         "${config.wayland.windowManager.sway.config.modifier}+Return" = "exec ${pkgs.kitty}/bin/kitty";
         "${config.wayland.windowManager.sway.config.modifier}+d" = "exec ${pkgs.fuzzel}/bin/fuzzel";
+        "${config.wayland.windowManager.sway.config.modifier}+q" = "kill";
+      };
+      output = {
+        "eDP-1" = {
+          pos = "0 0";
+          res = "1920x1080";
+        };
+        "DP-2" = {
+          pos = "1920 0";
+          res = "1920x1080";
+        };
+        "HDMI-A-1" = {
+          pos = "3840 0";
+          res = "1920x1080@74.973Hz";
+        };
       };
       startup = [
         {command = "kitty";}
